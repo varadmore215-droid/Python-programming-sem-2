81236f930fed54e6ada3f30a60b20f8c3f7b3926
varadmore215-droid <varadmore215@gmail.com>
2026-03-09 14:49:53 +0530

    Create Exp5.1.py

---
+++ Exp5/Exp5.1.py
@@ -0,0 +1,18 @@
+# Creating an empty dictionary
+
+
+student = {}
+# Adding key-value pairs
+student["name"] = "Rahul"
+student["age"] = 20
+student["course"] = "Python"
+print("Dictionary after adding elements:")
+print(student)
+# Updating a value
+student["age"] = 21
+print("\nDictionary after updating a value:")
+print(student)
+# Deleting a key-value pair
+del student["course"]
+print("\nDictionary after deleting a key-value pair:")
+print(student)
