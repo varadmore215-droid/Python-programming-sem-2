4380e17610b66042dbbe0c8d9cd5d48dd58e5f79
varadmore215-droid <varadmore215@gmail.com>
2026-03-09 15:50:26 +0530

    Create Act 3.py

---
+++ Exp4/Act 3.py
@@ -0,0 +1,13 @@
+# Reverse a customer feedback message.
+"""
+Created on Mon Mar  9 15:46:28 2026
+
+@author: Varad
+"""
+
+feedback = "Quick delivery"
+
+# join() merges the characters back together
+reversed_feedback = "".join(reversed(feedback))
+
+print(reversed_feedback)
