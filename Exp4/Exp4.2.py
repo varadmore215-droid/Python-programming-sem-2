adc73d215a827e213bbb659d5d01820189a56b33
varadmore215-droid <varadmore215@gmail.com>
2026-03-09 14:45:43 +0530

    Create Exp4.2.py

---
+++ Exp4/Exp4.2.py
@@ -0,0 +1,15 @@
+# python program to reverse the  string
+"""
+Created on Mon Mar  2 15:28:08 2026
+
+@author: varad
+
+"""
+# Take input from the user
+string = input("Enter a string: ")
+
+# Reverse the string using slicing
+reversed_string = string[::-1]
+
+# Print the result
+print("Reversed string:", reversed_string)
