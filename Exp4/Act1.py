190e746527ba1b82e6a1c7d09a52392d2a68ebc9
varadmore215-droid <varadmore215@gmail.com>
2026-03-09 14:22:11 +0530

    Create Act1.py

---
+++ Exp4/Act1.py
@@ -0,0 +1,16 @@
+# find average and topper marks
+"""
+Created on Mon Mar  2 15:34:18 2026
+
+@author: swapnil
+"""
+
+marks =  [85, 56, 78, 56, 90 ]
+
+average_marks =  sum(marks) / len(marks)
+
+topper_marks = max(marks)
+
+print(f"Average marks: {average_marks:.2f}")
+
+print(f"Topper marks: {topper_marks}")
