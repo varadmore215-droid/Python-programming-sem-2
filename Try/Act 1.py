e9e764675e898a11634993929f492c6cceb8109c
varadmore215-droid <varadmore215@gmail.com>
2026-03-14 18:46:44 +0530

    Create Act 1.py

---
+++ Try/Act 1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- comment
+"""
+Created on Mon Feb  9 10:30:56 2026
+
+@author: varad
+"""
+
+num1=int(input("Enter Number 1:"))
+num2=int(input("Enter Number 2:"))
+
+sum=num1+num2
+print("Addition=",sum)
