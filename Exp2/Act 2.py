997c26655721a43a738779d102893b445b904734
varadmore215-droid <varadmore215@gmail.com>
2026-03-16 15:04:15 +0530

    Create Act 2.py

---
+++ Exp2/Act 2.py
@@ -0,0 +1,15 @@
+#  Movie ticket eligibility program
+
+"""
+Created on Mon Mar 16 14:47:19 2026
+
+@author: Varad
+"""
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to enter the movie.")
+else:
+    print("Sorry, you are not eligible to enter the movie.")
