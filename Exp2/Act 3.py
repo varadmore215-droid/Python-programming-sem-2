ffce9d3a6426da0c2a5468708020a10aeec034e2
varadmore215-droid <varadmore215@gmail.com>
2026-03-16 15:04:56 +0530

    Create Act 3.py

---
+++ Exp2/Act 3.py
@@ -0,0 +1,13 @@
+# Bank loan approval program
+"""
+Created on Mon Mar 16 14:50:22 2026
+
+@author: Varad
+"""
+
+salary = float(input("Enter your monthly salary: "))
+
+if salary >= 25000:
+    print("Loan Approved")
+else:
+    print("Loan Rejected")
